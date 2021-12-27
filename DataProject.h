e1add67d62b8509cff761e95aeb1f5e0b3c4417f
Youssif Khaled Shawky <85262382+youssif1807198@users.noreply.github.com>
2021-12-27 23:32:59 +0200

    Delete DataProject.h

--- DataProject.h
+++
@@ -1,4 +0,0 @@
-void compression(vector<string>& StringArr, struct Table_Keys table[ ], int length_of_table);
-void convertToString(vector<char>& arrChar, vector<string>& newarrString, int size);
-void stringtochar(vector<string>& arrString, vector<char>& arrChar);
-void minifying(vector<string>& arrString)
